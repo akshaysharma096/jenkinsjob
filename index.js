d181fcec9c0b2ad2e91ac725d893120732253c38
Akshay Sharma <akshay.sharma@fresha.com>
2024-09-27 01:01:41 +0100

    Handle 502 gateway errors
    Signed-off-by: Akshay Sharma <akshay.sharma@fresha.com>

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-const request = require('request');
-const core = require('@actions/core');
+const request = require("request");
+const core = require("@actions/core");
+
+class BadGatewayError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "BadGatewayError";
+    this.statusCode = 502;
+  }
+}
 
 // create auth token for Jenkins API
-const API_TOKEN = Buffer.from(`${core.getInput('user_name')}:${core.getInput('api_token')}`).toString('base64');
+const API_TOKEN = Buffer.from(
+  `${core.getInput("user_name")}:${core.getInput("api_token")}`
+).toString("base64");
 
 let timer = setTimeout(() => {
   core.setFailed("Job Timeout");
   core.error("Exception Error: Timed out");
-  }, (Number(core.getInput('timeout')) * 1000));
+}, Number(core.getInput("timeout")) * 1000);
 
 const sleep = (seconds) => {
   return new Promise((resolve, reject) => {
-    setTimeout(resolve, (seconds * 1000));
+    setTimeout(resolve, seconds * 1000);
   });
 };
 
 // returns the queue item url from the response's location header
 async function triggerJenkinsJob(jobName, params) {
-  const jenkinsEndpoint = core.getInput('url');
+  const jenkinsEndpoint = core.getInput("url");
   const req = {
-    method: 'POST',
+    method: "POST",
     url: `${jenkinsEndpoint}/job/${jobName}/buildWithParameters`,
     form: params,
     headers: {
-      'Authorization': `Basic ${API_TOKEN}`
-    }
-  }
+      Authorization: `Basic ${API_TOKEN}`,
+    },
+  };
   return new Promise((resolve, reject) =>
     request(req, (err, res) => {
       if (err) {
@@ -35,7 +45,7 @@ async function triggerJenkinsJob(jobName, params) {
         reject();
         return;
       }
-      const location = res.headers['location'];
+      const location = res.headers["location"];
       if (!location) {
         const errorMessage = "Failed to find location header in response!";
         core.setFailed(errorMessage);
@@ -51,66 +61,89 @@ async function triggerJenkinsJob(jobName, params) {
 }
 
 async function getJobStatus(jobName, statusUrl) {
-  if (!statusUrl.endsWith('/'))
-    statusUrl += '/';
+  if (!statusUrl.endsWith("/")) statusUrl += "/";
 
   const req = {
-    method: 'GET',
+    method: "GET",
     url: `${statusUrl}api/json`,
     headers: {
-      'Authorization': `Basic ${API_TOKEN}`
-    }
-  }
+      Authorization: `Basic ${API_TOKEN}`,
+    },
+  };
   return new Promise((resolve, reject) =>
-      request(req, (err, res, body) => {
-        if (err) {
-          clearTimeout(timer);
-          reject(err);
-          return;
-        }
-        core.info(`The response code is ${res.statusCode}`);
-        core.info(`The body of response is: ${body}`);
-
+    request(req, (err, res, body) => {
+      if (err) {
+        clearTimeout(timer);
+        reject(err);
+        return;
+      }
+      core.info(`The response code from Jenkins API is: ${res.statusCode}`);
+      if (res.statusCode == 200) {
         resolve(JSON.parse(body));
-      })
-    );
+      } else {
+        throw new BadGatewayError(
+          `Wrong http response from host - ${res.statusCode}`
+        );
+      }
+    })
+  );
 }
 
 // see https://issues.jenkins.io/browse/JENKINS-12827
 async function waitJenkinsJob(jobName, queueItemUrl, timestamp) {
   const sleepInterval = 5;
-  let buildUrl = undefined
+  let buildUrl = undefined;
   core.info(`>>> Waiting for '${jobName}' ...`);
   while (true) {
     // check the queue until the job is assigned a build number
     if (!buildUrl) {
-      let queueData = await getJobStatus(jobName, queueItemUrl);
+      try {
+        let queueData = await getJobStatus(jobName, queueItemUrl);
 
-      if (queueData.cancelled)
-        throw new Error(`Job '${jobName}' was cancelled.`);
+        if (queueData.cancelled)
+          throw new Error(`Job '${jobName}' was cancelled.`);
 
-      if (queueData.executable && queueData.executable.url) {
-        buildUrl = queueData.executable.url; 
-        core.info(`>>> Job '${jobName}' started executing. BuildUrl=${buildUrl}`);
-      }
+        if (queueData.executable && queueData.executable.url) {
+          buildUrl = queueData.executable.url;
+          core.info(
+            `>>> Job '${jobName}' started executing. BuildUrl=${buildUrl}`
+          );
+        }
 
-      if (!buildUrl) {
-        core.info(`>>> Job '${jobName}' is queued (Reason: '${queueData.why}'). Sleeping for ${sleepInterval}s...`);
-        await sleep(sleepInterval);
-        continue;
+        if (!buildUrl) {
+          core.info(
+            `>>> Job '${jobName}' is queued (Reason: '${queueData.why}'). Sleeping for ${sleepInterval}s...`
+          );
+          await sleep(sleepInterval);
+          continue;
+        }
+      } catch (error) {
+        if (error instanceof BadGatewayError) {
+          core.info(`Received BadGatewayError for Job ${jobName}`);
+          await sleep(sleepInterval);
+          continue;
+        } else {
+          throw new Error(
+            `Unexpected error in Job '${buildData.fullDisplayName}',  ${error.message}`
+          );
+        }
       }
     }
 
     let buildData = await getJobStatus(jobName, buildUrl);
 
     if (buildData.result == "SUCCESS") {
-      core.info(`>>> Job '${buildData.fullDisplayName}' completed successfully!`);
+      core.info(
+        `>>> Job '${buildData.fullDisplayName}' completed successfully!`
+      );
       break;
     } else if (buildData.result == "FAILURE" || buildData.result == "ABORTED") {
       throw new Error(`Job '${buildData.fullDisplayName}' failed.`);
     }
 
-    core.info(`>>> Job '${buildData.fullDisplayName}' is executing (Duration: ${buildData.duration}ms, Expected: ${buildData.estimatedDuration}ms). Sleeping for ${sleepInterval}s...`);
+    core.info(
+      `>>> Job '${buildData.fullDisplayName}' is executing (Duration: ${buildData.duration}ms, Expected: ${buildData.estimatedDuration}ms). Sleeping for ${sleepInterval}s...`
+    );
     await sleep(sleepInterval); // API call interval
   }
 }
@@ -118,19 +151,21 @@ async function waitJenkinsJob(jobName, queueItemUrl, timestamp) {
 async function main() {
   try {
     let params = {};
-    let startTs = + new Date();
-    let jobName = core.getInput('job_name');
-    if (core.getInput('parameter')) {
-      params = JSON.parse(core.getInput('parameter'));
+    let startTs = +new Date();
+    let jobName = core.getInput("job_name");
+    if (core.getInput("parameter")) {
+      params = JSON.parse(core.getInput("parameter"));
       core.info(`>>> Parameter ${params.toString()}`);
     }
     // POST API call
     let queueItemUrl = await triggerJenkinsJob(jobName, params);
 
-    core.info(`>>> Job '${jobName}' was queued successfully. QueueUrl=${queueItemUrl}`);
+    core.info(
+      `>>> Job '${jobName}' was queued successfully. QueueUrl=${queueItemUrl}`
+    );
 
     // Waiting for job completion
-    if (core.getInput('wait') == 'true') {
+    if (core.getInput("wait") == "true") {
       await waitJenkinsJob(jobName, queueItemUrl, startTs);
     }
   } catch (err) {
@@ -141,5 +176,5 @@ async function main() {
   }
 }
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED="0";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 main();
